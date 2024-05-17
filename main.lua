976433f931f15578ad6439e70b724e3b2dcf4950
JJSax <5084938+JJSax@users.noreply.github.com>
2024-05-17 13:20:03 -0600

    Add culling and panning

--- main.lua
+++ main.lua
@@ -3,22 +3,30 @@ local lg = love.graphics
 local Tiles = require "libraries.luatile.grid"
 local tiles
 
-local canvas, mazedata, maze, startPoint
-local mazeImage, mazeData, currentPath
+local currentPath
+local MOVETIMER = 2
+local moveTimer = MOVETIMER
 
-local lineWidth = 2
 local boxSize = 8-- includes walls
-local pathColor = {1, 1, 1, 1}
+local pathColor = { 1, 1, 1, 1 }
+local moveSpeed = 150
+local offset = {x = 0, y = 0}
 
 function love.load()
-	mazeImage = lg.newImage("maze.png")
-	mazeData = love.image.newImageData("maze.png")
+	local mazeData = love.image.newImageData("maze.png")
 
 	-- Keep track of the current path
 	currentPath = {}
 
 	local dw, dh = mazeData:getDimensions()
-    tiles = Tiles.new(require("tile").new, dw / boxSize, dh / boxSize)
+	tiles = Tiles.new(require("tile").new, dw / boxSize, dh / boxSize)
+	for tx = 1, #tiles.tiles do
+		if not tiles(tx, 1).walls[1] then
+			currentPath[1] = tiles(tx, 1)
+			break
+		end
+
+	end
 
 	lg.setBackgroundColor(1,1,1)
 end
@@ -27,26 +35,43 @@ function love.update(dt)
 	-- Your pathfinding algorithm here, updating currentPath
 	-- For simplicity, I'm just updating it randomly here
 	if love.timer.getTime() % 2 < 1 then
-		table.insert(currentPath, {x = math.random(1, mazeImage:getWidth()), y = math.random(1, mazeImage:getHeight())})
+		-- table.insert(currentPath, {x = math.random(1, mazeImage:getWidth()), y = math.random(1, mazeImage:getHeight())})
 	else
-		table.remove(currentPath)
+		-- table.remove(currentPath)
+	end
+
+	if love.keyboard.isDown("w") then
+		offset.y = offset.y + moveSpeed * dt
+	end
+	if love.keyboard.isDown("a") then
+		offset.x = offset.x + moveSpeed * dt
+	end
+	if love.keyboard.isDown("s") then
+		offset.y = offset.y - moveSpeed * dt
+	end
+	if love.keyboard.isDown("d") then
+		offset.x = offset.x - moveSpeed * dt
 	end
 end
 
 function love.draw()
-    -- lg.draw(mazeImage, 0, 0)
-	for cell, x, y in tiles:iterate() do
-		cell:draw()
+	lg.push("all")
+	lg.translate(offset.x, offset.y)
+
+	local DBG = 0
+    for cell, x, y in tiles:iterate() do
+        local tx, ty = lg.transformPoint(cell.vx, cell.vy)
+        if tx + boxSize > 0 and tx < lg.getWidth()
+		and ty + boxSize > 0 and ty < lg.getHeight() then
+			DBG = DBG + 1
+			cell:draw()
+		end
 	end
 
-	-- Visualize the current path
-	-- for _, point in ipairs(currentPath) do
-	-- 	mazeData:setPixel(point.x, point.y, 255, 0, 0) -- Red color for the path
-	-- end
-
 	-- Apply changes to the displayed image
-	-- lg.clear()
-	-- lg.draw(lg.newImage(mazeData))
+    lg.pop()
+	lg.setColor(0,0,0)
+	lg.print(DBG)
 end
 
 function love.keypressed(key) end
