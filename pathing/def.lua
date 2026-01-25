606d7ba2f4cc4a5bdccc8b765aa4b921cefd8330
JJSax <5084938+JJSax@users.noreply.github.com>
2026-01-25 15:41:04 -0700

    Optimize DEF

--- pathing/def.lua
+++ pathing/def.lua
@@ -8,71 +8,22 @@ local Pathfinder = require "libraries.luatile.pathfinder"
 
 local def = setmetatable({}, { __index = Pathfinder })
 def.__index = def
-def._VERSION = "0.1.1"
+def._VERSION = "0.2.0"
 
+local dirMap = {
+	{ x = 0,  y = -1 },
+	{ x = 1,  y = 0 },
+	{ x = 0,  y = 1 },
+	{ x = -1, y = 0 }
+}
 
 ---Create the def
 function def.create(grid, startTile, target)
 	local self = setmetatable(Pathfinder.new(grid, startTile, target), def)
-	return self
-end
 
--- function def:nextPosSearch()
--- 	local x, y = self.currentTile.x, self.currentTile.y
--- 	if x > #self.grid.tiles then
--- 		x = 1
--- 		y = y + 1
--- 	end
--- 	if y > #self.grid.tiles[1] then
--- 		return self.grid(1, 1)
--- 	end
-
--- 	self.currentTile.x = x + 1
--- 	return self.currentTile
--- end
-
-function def:markDeadEnd(tile)
-	tile.deadEnd = true
-	tile.color = { .2, 0.22, 0.22 }
-	cfg.timerReset = cfg.DEADENDTIMER
-end
-
-local dirMap = {
-	{x = 0, y = -1},
-	{x = 1, y = 0},
-	{x = 0, y = 1},
-	{x = -1, y = 0}
-}
-
-local function constructPath(self)
-	local currentTile = self.start
-	local path = {self.start}
-	local inPath = {[self.start] = true}
-	repeat
-		for i, isWall in ipairs(currentTile.walls) do
-			if not isWall then
-				local dir = dirMap[i]
-				local nxt = self.grid:isValidCell(currentTile.x + dir.x, currentTile.y + dir.y)
-				if nxt and not inPath[nxt] and not self.visited[nxt] then
-					table.insert(path, nxt)
-					inPath[nxt] = true
-					currentTile = nxt
-					break
-				end
-			end
-		end
-	until currentTile == self.target
-	return path
-end
-
----Runs Single step through the def
-function def:step()
-	local nextMark = {}
+	local singlePaths = {}
 	for x, v in ipairs(self.grid.tiles) do
 		for y, tile in ipairs(v) do
-			if self.visited[tile] then
-				goto continue
-			end
 			local open = 0
 			for dir, wall in ipairs(tile.walls) do
 				if not wall then
@@ -83,26 +34,75 @@ function def:step()
 				end
 			end
 			if open <= 1 then
-				table.insert(nextMark, {x = x, y = y})
+				table.insert(singlePaths, tile)
 			end
-		    ::continue::
 		end
 	end
 
-	for _, v in ipairs(nextMark) do
-		local tile = self.grid(v.x, v.y)
-		self:markDeadEnd(tile)
-		self.visited[tile] = true
-	end
+	self.list = singlePaths
+	return self
+end
 
-	if #nextMark == 0 then
-		self.complete = true
-		self.path = constructPath(self)
-	end
+function def:markDeadEnd(tile)
+	tile.deadEnd = true
+	tile.color = { .2, 0.22, 0.22 }
+end
+
+function def:exploreTile(tile)
+	tile.color = { 0, 1, 1 }
+	cfg.timerReset = cfg.MOVETIMER
+end
+
+local function constructPath(self)
+	local currentTile = self.start
+	local path = {self.start}
+	local inPath = {[self.start] = true}
+	repeat
+		for i, tile in ipairs(currentTile:getNeighbors()) do
+			if not inPath[tile] and not tile.deadEnd then
+				currentTile = tile
+				table.insert(path, currentTile)
+				inPath[tile] = true
+				self:exploreTile(tile)
+				break
+			end
+		end
+	until currentTile == self.target
+	self.complete = true
+	self.path = path
+	return path
 end
 
 function def:step()
+	local nList = {}
+	for i, tile in ipairs(self.list) do
+		-- mark current tile as dead end
+		if tile ~= self.start and tile ~= self.target then
+			self:markDeadEnd(tile)
+		end
 
+		-- look at adjacent tiles for tiles that will have 0 ways to go after this tile is marked as a dead end
+		for _, adjTile in ipairs(tile:getNeighbors()) do
+			-- local adjTile = self.grid(tile.x + dirMap[dir].x, tile.y + dirMap[dir].y)
+			local options = 0
+			for _, d in ipairs(adjTile:getNeighbors()) do
+				if not d.deadEnd then
+					options = options + 1
+				end
+			end
+
+			if options == 1 and tile ~= self.start and tile ~= self.target then
+				-- if it will be a dead end, add it to self.list
+				table.insert(nList, adjTile)
+			end
+		end
+		self.list = nList
+	end
+	cfg.timerReset = cfg.DEADENDTIMER
+
+	if #self.list == 0 then
+		constructPath(self)
+	end
 end
 
 return def
