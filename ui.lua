84454f6314f3e3aa0f21fa376e3fcd447308bc06
JJSax <5084938+JJSax@users.noreply.github.com>
2026-02-08 14:13:45 -0700

    Add algorithm context menu

--- ui.lua
+++ ui.lua
@@ -1,12 +1,21 @@
-
 local geometry = require "geometry"
 local common = require "common"
+local cfg = require "settings"
+table.unpack = unpack or table.unpack
 
 local ui = {}
 ui.__index = ui
 
 local lg = love.graphics
 local font = lg.newFont(28)
+
+local contextMenu = nil
+local contextFont = lg.newFont(18)
+local height = 100
+
+local function getButtonXLeft(button)
+	return button.x * lg.getWidth() - button.width / 2
+end
 
 local function getPathingOptions()
 	local out = {}
@@ -25,10 +34,23 @@ local function run(button)
 	common.run = button.toggled
 end
 
+local function selectAlgorithm(button)
+	local bx = getButtonXLeft(button)
+	local ch = lg.getHeight() - height / 2
+	local h = contextFont:getHeight() * (#button.menuOptions + 1)
+	contextMenu = {
+		options = button.menuOptions,
+		x = bx,
+		y = ch - button.height / 2 - h,
+		width = button.width,
+		height = h
+	}
+end
+
 local function new()
 	local self = setmetatable({}, ui)
 
-	self.height = 100
+	height = 100
 
 	local aWidth = 175
 	local aHeight = 50
@@ -42,7 +64,7 @@ local function new()
 			text = "Algorithm",
 			color = { 0.6, 0.6, 0.6, 1 },
 			menuOptions = getPathingOptions(),
-			callback = function(n) end
+			callback = selectAlgorithm
 		},
 		genMaze = {
 			x = 0.8,
@@ -68,10 +90,6 @@ local function new()
 	return self
 end
 
-local function getButtonXCenter(button)
-	return button.x * lg.getWidth() - button.width / 2
-end
-
 function ui:update(dt)
 
 end
@@ -80,11 +98,11 @@ function ui:draw()
 	lg.push("all")
 	lg.setFont(font)
 	lg.setColor(0.4, 0.4, 0.4, 1)
-	lg.rectangle("fill", 0, lg.getHeight() - self.height, lg.getWidth(), self.height)
+	lg.rectangle("fill", 0, lg.getHeight() - height, lg.getWidth(), height)
 
-	local ch = lg.getHeight() - self.height / 2
+	local ch = lg.getHeight() - height / 2
 	for k, button in pairs(self.buttons) do
-		local bx = getButtonXCenter(button)
+		local bx = getButtonXLeft(button)
 
 		lg.setColor(button.color)
 		lg.rectangle("fill", bx, ch - button.height/2, button.width, button.height)
@@ -92,19 +110,61 @@ function ui:draw()
 		lg.print(button.text, bx + button.width/2 - font:getWidth(button.text)/2, ch - font:getHeight()/2)
 
 		local mx, my = love.mouse.getPosition()
-		if geometry.inRect(mx, my, bx, ch - button.height/2, button.width, button.height) then
+		if not contextMenu and geometry.inRect(mx, my, bx, ch - button.height/2, button.width, button.height) then
 			lg.setColor(0, 0, 0, 0.1)
 			lg.rectangle("fill", bx, ch - button.height / 2, button.width, button.height)
 		end
 	end
+
+	if contextMenu then
+		lg.setColor(0.8, 0.8, 0.8, 1)
+		lg.rectangle("fill", contextMenu.x, contextMenu.y, contextMenu.width, contextMenu.height)
+		local padding = 4  -- or whatever padding you want
+		lg.setColor(1, 1, 1)
+		for i, option in ipairs(contextMenu.options) do
+
+			-- check if mouse if over option
+			local mx, my = love.mouse.getPosition()
+			local rect = { contextMenu.x, contextMenu.y + (i - 1) * (contextFont:getHeight() + padding) + padding, contextMenu.width, contextFont:getHeight() }
+			if geometry.inRect(mx, my, table.unpack(rect)) then
+				lg.setColor(0, 0, 0, 0.1)
+				lg.rectangle("fill", table.unpack(rect))
+				lg.setColor(1, 1, 1)
+			end
+
+			local y = contextMenu.y + (i - 1) * (contextFont:getHeight() + padding)
+			lg.print(option, contextMenu.x + 10, y)
+		end
+	end
+
 	lg.pop()
 end
 
 function ui:mousepressed(x, y, b)
 	if b ~= 1 then return end
-	local ch = lg.getHeight() - self.height / 2
+
+	-- when there is a context menu, check if the click is in bounds and if not, make context menu nil
+	if contextMenu then
+		if not geometry.inRect(x, y, contextMenu.x, contextMenu.y, contextMenu.width, contextMenu.height) then
+			contextMenu = nil
+			return
+		end
+
+		local padding = 4
+		for i, option in ipairs(contextMenu.options) do
+			local rect = { contextMenu.x, contextMenu.y + (i - 1) * (contextFont:getHeight() + padding) + padding, contextMenu.width, contextFont:getHeight() }
+			if geometry.inRect(x, y, table.unpack(rect)) then
+				cfg.pathfinder = option
+				common.generateMaze(true)
+				contextMenu = nil
+				return
+			end
+		end
+	end
+
+	local ch = lg.getHeight() - height / 2
 	for k, button in pairs(self.buttons) do
-		local bx = getButtonXCenter(button)
+		local bx = getButtonXLeft(button)
 		local mx, my = love.mouse.getPosition()
 		if geometry.inRect(mx, my, bx, ch - button.height / 2, button.width, button.height) then
 			button.callback(button)
